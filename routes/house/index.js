36519f98dc22bdfe66808a07de7cf18273b15be8
Ciputra Pangestu <ciputrapangestu2810@gmail.com>
2020-07-21 21:50:27 +0700

    add filter database by title

--- routes/house/index.js
+++ routes/house/index.js
@@ -6,5 +6,6 @@ router.post('/upload', require('./controller').uploadHouse);
 router.delete('/remove/:title', require('./controller').deleteHouse);
 router.get('/', require('./controller').getHouse);
 router.put('/update/:id', require('./controller').updateHouse);
+router.get('/detail', require('./controller').filterHouse);
 
 module.exports = router;
